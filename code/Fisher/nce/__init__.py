8533cf9a96ffe897be909b40b874dce98a9a7ca9
karush17 <karushsuri@gmail.com>
2020-11-13 15:18:20 -0500

    start project

--- code/Fisher/nce/__init__.py
+++
@@ -1,3 +0,0 @@
-from nce.index_linear import IndexLinear
-from nce.index_gru import IndexGRU
-from nce.nce_loss import NCELoss
--- code/Fisher/test/test_evaluation.py
+++
@@ -1,6 +0,0 @@
-from main import evaluate, corpus
-
-
-def test_evaluate():
-    assert corpus.test
-    assert evaluate(corpus.test)
